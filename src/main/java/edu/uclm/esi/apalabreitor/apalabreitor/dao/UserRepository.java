cd50ce20a789c53f497f665c8177f0db7a5ed3cb
AurelioCamposGonzalez <55980808+AurelioCamposGonzalez@users.noreply.github.com>
2019-11-04 19:25:46 +0100

    Base de datos

--- src/main/java/edu/uclm/esi/apalabreitor/apalabreitor/dao/UserRepository.java
+++ src/main/java/edu/uclm/esi/apalabreitor/apalabreitor/dao/UserRepository.java
@@ -1,10 +1,14 @@
 package edu.uclm.esi.apalabreitor.apalabreitor.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import edu.uclm.esi.apalabreitor.apalabreitor.model.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, String>{
 
+	User findByuserName (String userName);
 	User findByEmail(String email);
+	User findBypwd (String pwd);
 }
